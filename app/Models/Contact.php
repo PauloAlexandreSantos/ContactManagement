84287a4be9c9eb076026797abf65fa6985a0836e
PauloAlexandreSantos <tecnicopaulo@outlook.pt>
2021-05-29 10:42:10 +0100

    model finish

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     use HasFactory;
+    protected $table = 'contact';
+    protected $fillable = ['id', 'name', 'email', 'contact', 'deleted_at', 'created_at', 'updated_at'];
 }
