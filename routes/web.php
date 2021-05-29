0c8aa3ae018e1dbd5637b634e8ac5667d0abcaa4
PauloAlexandreSantos <tecnicopaulo@outlook.pt>
2021-05-29 10:51:29 +0100

    routes finish

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//contacts start
+route::get('/contact/home', ['as' => 'site.contact.index', 'uses' => 'ContactController@index']);
+route::get('/details/{id}', ['as' => 'site.contact.details', 'uses' => 'ContactController@show']);
+
+route::get('/contact/create', ['as' => 'site.contact.create', 'uses' => 'ContactController@create']);
+route::post('/contact/store', ['as' => 'site.contact.store', 'uses' => 'ContactController@store']);
+
+route::get('/contact/edit/{id}', ['as' => 'site.contact.edit', 'uses' => 'ContactController@edit']);
+route::put('/contact/update/{id}', ['as' => 'site.contact.update', 'uses' => 'ContactController@update']);
+
+route::get('/contact/delete/{id}', ['as' => 'site.contact.delete', 'uses' => 'ContactController@destroy']);
+//contacts end
